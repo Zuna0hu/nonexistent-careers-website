4b9fbadaf13973b0e5fa2eb2929137c583b3e80e
Zunao Hu <124393973+Zuna0hu@users.noreply.github.com>
2025-01-08 01:27:51 +0000

    Initialize flask application in app.py
    Add an easy flask application in app.py.
    
    pyproject.toml is the configuration file used for specifying build
    system requirements, dependencies, and project metadata.

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(
+    __name__
+)  # If you run a Python file directly (e.g., python app.py), Python sets __name__ to "__main__"
+# __name__ is passed to the Flask class to indicate the location of the current module
+# Flask(__name__) registers it as a WSGI application so it can communicate with web servers
+
+
+@app.route("/")
+def hello_world():
+  return "Hello World!"
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)  # This runs only if the script is executed directly (e.g., python app.py)
