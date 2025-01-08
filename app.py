8ec797bde53c5505ea066f1fa1aec7f8516c911b
Zunao Hu <124393973+Zuna0hu@users.noreply.github.com>
2025-01-08 04:25:06 +0000

    Initialize home.html and link it to app.py
    home.html is the home page for our website.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(
     __name__
@@ -9,7 +9,7 @@ app = Flask(
 
 @app.route("/")
 def hello_world():
-  return "Hello World!"
+  return render_template('home.html')
 
 
 if __name__ == "__main__":
