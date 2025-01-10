a72724ed8d4bf07fa777c58474125566a1b172dc
Zunao Hu <124393973+Zuna0hu@users.noreply.github.com>
2025-01-10 19:19:46 +0000

    Add emulated database using Python list
    This is just to showcase how the database will work in our dynamic web
    design. Later, this code will be commented out and replaced by a real
    database structure.

--- app.py
+++ app.py
@@ -6,6 +6,33 @@ app = Flask(
 # __name__ is passed to the Flask class to indicate the location of the current module
 # Flask(__name__) registers it as a WSGI application so it can communicate with web servers
 
+# this is an imitation of the database structure we are going to use
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Remote',
+    'salary': '$50,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Remote',
+    'salary': '$100,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Montreal, Canada',
+    'salary': '$50,000'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$100,000'
+  }
+]
 
 @app.route("/")
 def hello_world():
